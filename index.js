472c173bffd4f5deef5e9f54053d63d3cf979962
Abdallah-Gh1 <140517773+Abdallah-Gh1@users.noreply.github.com>
2023-08-10 16:08:23 +0300

    to do list

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-function AddTask(){
+function AddTask() {
+  var intp = document.getElementById("input").value;
+  const node = document.createElement("li");
+  const btn = document.createElement("button");
+  const text = document.createTextNode(intp);
+  node.appendChild(text);
+  btn.classList.add("delete");
+  btn.innerHTML = "Delete";
+  btn.id = "deleteB";
+  node.appendChild(btn);
+  document.getElementById("ul").appendChild(node);
+  btn.addEventListener("click", del);
+  document.getElementById("input").value = "";
+  function del() {
 
-    var intp = document.getElementById("input").value;
-    const node = document.createElement("li");
-    const btn = document.createElement("button");
-    const text = document.createTextNode(intp);
-    node.appendChild(text);
-    btn.classList.add("delete");
-    btn.innerHTML = "Delete";
-    btn.id = "deleteB"
-    node.appendChild(btn);
-
-    document.getElementById("ul").appendChild(node);
-  
-    btn.addEventListener('click',del)
-    document.getElementById("input").value = "";  
-function del(){
     node.remove();
-}
+
+  }
 }
